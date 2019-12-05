3b029dcd4aa4fdd15841a661decce45ce53a5dd1
Jan Carba <xcarba00@stud.fit.vutbr.cz>
2019-12-05 14:59:36 +0100

    HELLO C++ WORLD
    YOU BIAČ

---
+++ cmake-build-debug/CMakeFiles/ims_proj.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ims_proj.dir/main.cpp.o"
+  "ims_proj.exe"
+  "ims_proj.pdb"
+  "libims_proj.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ims_proj.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Ahoj Jirko ;]" << std::endl;
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
